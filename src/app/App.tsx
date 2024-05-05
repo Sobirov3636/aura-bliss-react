acf7551ba4bbc3e86d06937d0d4e32a30615900d
Sobirov3636 <sobirov3636@gmail.com>
2024-05-05 23:12:15 +0900

    feat: create routing system & develop screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,24 +2,32 @@ import React from "react";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import "../css/app.css";
 import { RippleBadge } from "./MaterialTheme/styled";
+import { Route, Switch } from "react-router-dom";
+import ProductsPage from "./screens/productsPage";
+import OrdersPage from "./screens/ordersPage";
+import UserPage from "./screens/userPage";
+import HelpPage from "./screens/helpPage";
+import HomePage from "./screens/homePage";
 
 function App() {
   return (
-    <Container sx={{ background: "orange" }}>
-      <Stack flexDirection={"column"}>
-        <Box sx={{ my: 4 }}>
-          {" "}
-          <Typography variant='h4' component={"h4"}>
-            Create React App on Typescript whith REDUX
-          </Typography>{" "}
-        </Box>
-        <Box>
-          <RippleBadge badgeContent={4}>
-            <Button variant='contained'>Contained</Button>
-          </RippleBadge>
-        </Box>
-      </Stack>
-    </Container>
+    <Switch>
+      <Route path='/products'>
+        <ProductsPage />
+      </Route>
+      <Route path='/orders'>
+        <OrdersPage />
+      </Route>
+      <Route path='/member-page'>
+        <UserPage />
+      </Route>
+      <Route path='/help'>
+        <HelpPage />
+      </Route>
+      <Route path='/'>
+        <HomePage />
+      </Route>
+    </Switch>
   );
 }
 
