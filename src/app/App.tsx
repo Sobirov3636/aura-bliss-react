c404cd7aa2b30e52fac63dd77c2d1d8e221394cc
Sobirov3636 <sobirov3636@gmail.com>
2024-05-05 23:20:42 +0900

    feat: organize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,32 +2,41 @@ import React from "react";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import "../css/app.css";
 import { RippleBadge } from "./MaterialTheme/styled";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import ProductsPage from "./screens/productsPage";
 import OrdersPage from "./screens/ordersPage";
 import UserPage from "./screens/userPage";
 import HelpPage from "./screens/helpPage";
 import HomePage from "./screens/homePage";
+import Footer from "./components/footer";
+import HomeNavbar from "./components/headers/HomeNavbar";
+import OtherNavbar from "./components/headers/OtherNavbar";
 
 function App() {
+  const location = useLocation();
   return (
-    <Switch>
-      <Route path='/products'>
-        <ProductsPage />
-      </Route>
-      <Route path='/orders'>
-        <OrdersPage />
-      </Route>
-      <Route path='/member-page'>
-        <UserPage />
-      </Route>
-      <Route path='/help'>
-        <HelpPage />
-      </Route>
-      <Route path='/'>
-        <HomePage />
-      </Route>
-    </Switch>
+    <>
+      {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
+
+      <Switch>
+        <Route path='/products'>
+          <ProductsPage />
+        </Route>
+        <Route path='/orders'>
+          <OrdersPage />
+        </Route>
+        <Route path='/member-page'>
+          <UserPage />
+        </Route>
+        <Route path='/help'>
+          <HelpPage />
+        </Route>
+        <Route path='/'>
+          <HomePage />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
   );
 }
 
