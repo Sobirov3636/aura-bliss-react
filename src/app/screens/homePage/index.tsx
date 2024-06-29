4aef4107eff35c56a3c789321c251dea58e3aef5
Sobirov3636 <sobirov3636@gmail.com>
2024-06-29 22:14:17 +0500

    feat: develop slice and selector usage

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -4,13 +4,25 @@ import NewArrivals from "./NewArrivals";
 import Advertisement from "./Advertisement";
 import ProductCategories from "./ProductCategories";
 import Events from "./Events";
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+
+import { retrieveBestSellers } from "./selector";
+import { Product } from "../../../lib/types/product";
+import { setBestSellers } from "./store";
+
+/** REDUX SLICE & SELECTOR **/
+const actionDispatch = (dispatch: Dispatch) => ({
+  setBestSellers: (data: Product[]) => dispatch(setBestSellers(data)),
+});
+const bestSellersRetriever = createSelector(retrieveBestSellers, (bestSellers) => ({ bestSellers }));
 
 function HomePage() {
-  // Selector: Store => Data
-  useEffect(() => {
-    // Backend server data request => Data
-    // slice: Data => Store
-  }, []);
+  const { setBestSellers } = actionDispatch(useDispatch());
+  const { bestSellers } = useSelector(bestSellersRetriever);
+
+  useEffect(() => {}, []);
   return (
     <div className='homepage'>
       <BestSellers />
